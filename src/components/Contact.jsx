de6d93f0c206cb163722162201607a7496f142fe
Abeera Tahir <abeeratahir2018@gmail.com>
2024-03-11 11:56:50 +0500

    Fix contact section styling for mobile devices

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,8 +31,8 @@ const Contact = () => {
   return (
     <div className="flex justify-center items-center">
       <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
-        className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
+        variants={slideIn("left", "", 0.2, 1)}
+        className="w-[97%] lg:w-[65%] bg-black-100 p-8 rounded-2xl">
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
 
@@ -62,7 +62,7 @@ const Contact = () => {
               value={form.name}
               onChange={handleChange}
               placeholder="What's your good name?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium text-sm sm:text-[1rem]"
             />
           </label>
           <label className="flex flex-col">
@@ -72,8 +72,8 @@ const Contact = () => {
               name="email"
               value={form.email}
               onChange={handleChange}
-              placeholder="What's your web address?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              placeholder="What's your email address?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium text-sm sm:text-[1rem]"
             />
           </label>
           <label className="flex flex-col">
@@ -84,7 +84,7 @@ const Contact = () => {
               value={form.message}
               onChange={handleChange}
               placeholder="What you want to say?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium text-sm sm:text-[1rem]"
             />
           </label>
 
