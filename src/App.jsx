f666e782d5b2c506884863459452bf9d70826fb4
Abeera Tahir <abeeratahir2018@gmail.com>
2024-04-14 13:32:56 +0500

    Update toaster and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
-import {
-  About,
-  Contact,
-  Hero,
-  Navbar,
-  Tech,
-  Works,
-  StarsCanvas,
-} from "./components";
-import { Toaster } from "react-hot-toast";
+import { About, Contact, Hero, Navbar, Tech, Works } from "./components";
 
 function App() {
   return (
-    <>
-      <Toaster />
-      <BrowserRouter>
-        <div className="relative z-0 bg-primary">
-          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            <Navbar />
-            <Hero />
-          </div>
-          <About />
-          <Works />
-          <Tech />
-          <div className="relative z-0">
-            <Contact />
-            <StarsCanvas />
-          </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
         </div>
-      </BrowserRouter>
-    </>
+        <About />
+        <Works />
+        <Tech />
+        <div className="relative z-0">
+          <Contact />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,8 @@
-import { StarsCanvas } from "./canvas";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
 import Tech from "./Tech";
 import Works from "./Works";
 import Contact from "./Contact";
-import CanvasLoader from "./Loader";
 
-export { Hero, Navbar, About, Tech, Works, Contact, CanvasLoader, StarsCanvas };
+export { Hero, Navbar, About, Tech, Works, Contact };
